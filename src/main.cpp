ec2367523f7c21497aac901c7f59934790bc02f3
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-05-06 21:24:46 -0300

    updated main description

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+/* Project by Anderson Kmetiuk
+   Board --> ESP32 WT32 ETH01
+   The main purpose of this repo is to test the basic features of the board
+   and try simple projects just to get use to
+*/
 #include <Arduino.h>
 
 void setup() {
