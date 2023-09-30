6c1663ab2cfc778addf02f1b5a36658c3087fa00
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-09-30 19:27:45 -0300

    hello-world

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,31 @@
 */
 #include <Arduino.h>
 
+// defines
+#define LED1 14 //LED Digital Port 7
+#define LED2 15 // LED Digital Port 8
+
 void setup() {
-  Serial.begin(921600);
-  Serial.println("Inicio");
+  //Pins Setup
+  pinMode(LED1,OUTPUT);
+  digitalWrite(LED1, LOW);
+  pinMode(LED2,OUTPUT);
+  digitalWrite(LED2, LOW);
+
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("Serial Begin...");
 
 }
 
 void loop() {
-  delay(2000);
-  Serial.println("Hello");
+  //HELLO WORLD
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,LOW);
+  Serial.println("LED1 ON - LED2 OFF");
+  delay(3000);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  Serial.println("LED1 OFF - LED2 ON");
+  delay(3000);
 }
