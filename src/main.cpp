90d2ad08fe5b8afee00509c48d58422e9212f090
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-10-03 21:27:00 -0300

    test-led

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,91 @@
-/* Project by Anderson Kmetiuk
-   Board --> ESP32 WT32 ETH01
-   The main purpose of this repo is to test the basic features of the board
-   and try simple projects just to get use to
+/*Project by Anderson Kmetiuk
+  Board --> ESP32 WT32 ETH01 
 */
 #include <Arduino.h>
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
-#include "BluetoothSerial.h"
-
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
+// defines
+#define LED1 14 // IO14
+#define LED2 15 // IO15
+#define LED3 12 // IO12
+#define LED4 32 // IO32
+#define LED5 4 // IO4
+#define LED6 2 // IO2
+#define BUTTON 36 //Input Only Pins - IO35, IO36 or IO39
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  //Pins Setup
+  pinMode(LED1,OUTPUT);
+  digitalWrite(LED1, LOW);
+  pinMode(LED2,OUTPUT);
+  digitalWrite(LED2, LOW);
+  pinMode(LED3,OUTPUT);
+  digitalWrite(LED3, LOW);
+  pinMode(LED4,OUTPUT);
+  digitalWrite(LED4, LOW);
+  pinMode(LED5,OUTPUT);
+  digitalWrite(LED5, LOW);
+  pinMode(LED6,OUTPUT);
+  digitalWrite(LED6, LOW);
+
+  Serial.begin(9600);
+  delay(5000);
+  Serial.println("Serial Begin...");
+
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
+  //LED1
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED1");
+  delay(3000);
+  //LED2
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED2");
+  delay(3000);
+  //LED3
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED3");
+  delay(3000);
+  //LED4
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED4");
+  delay(3000);
+  //LED3
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, HIGH);
+  digitalWrite(LED6, LOW);
+  Serial.println("LED5");
+  delay(3000);
+  //LED3
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, HIGH);
+  Serial.println("LED6");
+  delay(3000);
 }
