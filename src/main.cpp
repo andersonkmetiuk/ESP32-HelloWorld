ca77e9826c0e8fbf6f39350b39eaf28190946cc2
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-10-15 22:57:30 -0300

    wifi-config

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,24 @@
    and try simple projects just to get use to
 */
 #include <Arduino.h>
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
-//
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+#include <WiFi.h>
+#include <Update.h>
+#include "secrets.h"
 
-#include "BluetoothSerial.h"
-
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
+const char* ssid = NETWORK_NAME;
+const char* password =  NETWORK_PASSWORD;
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+ Serial.begin(115200);
+ WiFi.begin(ssid, password);
+ 
+ while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.println("Connecting to WiFi..");
+ }
+ Serial.println("Connected to the WiFi network");
+ Serial.println("Endereço de IP: ");
+ Serial.println(WiFi.localIP());
 }
-
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
-}
+ 
+void loop() {}
