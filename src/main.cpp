23e6171f0cfaa8ae8bd95b25286eec996e3ac3f0
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2024-05-07 15:05:49 -0300

    Ethernet+OTA+Telnet

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,97 @@
 #include <ArduinoOTA.h>
 #include <WiFi.h>
 #include <TelnetStream.h>
-
-#define LED 14
-#define LEDTIME 500
+#include <ETH.h>
 
 const char ssid[] = SECRET_SSID;    // your network SSID (name)
 const char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
-void setup() {
+unsigned long loopCount;
+#define LOOPSEC 15000
+
+static bool eth_connected = false;
+
+void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      Serial.print("ETH MAC: ");
+      Serial.print(ETH.macAddress());
+      Serial.print(", IPv4: ");
+      Serial.print(ETH.localIP());
+      if (ETH.fullDuplex()) {
+        Serial.print(", FULL_DUPLEX");
+      }
+      Serial.print(", ");
+      Serial.print(ETH.linkSpeed());
+      Serial.println("Mbps");
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port)
+{
+  TelnetStream.print("\nconnecting to ");
+  TelnetStream.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    TelnetStream.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    TelnetStream.write(client.read());
+  }
+
+  TelnetStream.println("\n\nclosing connection...\n");
+  client.stop();
+}
+
+void setup()
+{
   Serial.begin(115200);
 
   //WIFI
-  Serial.print("Attempting to connect to WPA SSID: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  // Wait for WiFi connection
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Failed to connect.");
-    while (1) {
-      delay(10);
-    }
-  }
+  // Serial.print("Attempting to connect to WPA SSID: ");
+  // Serial.println(ssid);
+  // WiFi.begin(ssid, pass);
+  // Serial.println("Connecting");
+  // // Wait for WiFi connection
+  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  //   Serial.println(".");
+  //   delay(100);
+  // }
 
-  IPAddress ip = WiFi.localIP();
-  Serial.println();
-  Serial.println("Connected to WiFi network.");
-  Serial.print("Connect with Telnet client to ");
-  Serial.println(ip);
+  // IPAddress ip = WiFi.localIP();
+  // Serial.println();
+  // Serial.println("Connected to WiFi network.");
+  // Serial.print("Connect with Telnet client to ");
+  // Serial.println(ip);
+
+
+  WiFi.onEvent(WiFiEvent);
+  ETH.begin();
 
   TelnetStream.begin();
 
@@ -45,35 +109,29 @@ void setup() {
   ArduinoOTA.setHostname("ESP32-OTA");
   ArduinoOTA.begin();
 
-  // LED Setup
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED, LOW);
+  loopCount = millis() + LOOPSEC;
+  delay(15000);
+  TelnetStream.println("Begin...");
 }
 
-void loop() {
+
+void loop()
+{
   //OTA Handler
   ArduinoOTA.handle();
 
-  switch (TelnetStream.read()) {
-    case 'R':
-    TelnetStream.stop();
-    delay(100);
-    ESP.restart();
-      break;
-    case 'C':
-      TelnetStream.println("bye bye");
-      TelnetStream.stop();
-      break;
+  if (millis() > loopCount) {
+    TelnetStream.println("\n\n\n\n========================================================\n");
+    TelnetStream.print("MAC: ");
+    TelnetStream.println(ETH.macAddress());
+    TelnetStream.print("IP: ");
+    TelnetStream.println(ETH.localIP());
+    TelnetStream.print(ETH.linkSpeed());
+    TelnetStream.println("Mbps");
+    TelnetStream.println("\n========================================================\n\n");
+    testClient("google.com", 80);
+    loopCount = millis() + LOOPSEC;
   }
-
-  //Simple Test
-  TelnetStream.println("LED ON");
-  digitalWrite(LED, HIGH);
-  delay(LEDTIME);
-  TelnetStream.println("LED OFF");
-  TelnetStream.println("--------");
-  digitalWrite(LED,LOW);
-  delay(LEDTIME);
 }
 
 
